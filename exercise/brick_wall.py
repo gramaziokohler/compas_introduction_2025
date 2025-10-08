0d0ba952a9758a15403dcf8ffac98e8656eec316
Gonzalo Casas <casas@arch.ethz.ch>
2025-10-08 14:05:17 +0200

    Add brick wall exercise solution

--- exercise/brick_wall.py
+++ exercise/brick_wall.py
@@ -1,6 +1,6 @@
 
 import math
-from compas.geometry import Point
+from compas.geometry import Frame
 from compas.geometry import Box
 
 # There's a data type called Brick
@@ -11,19 +11,16 @@ class Brick:
         self.length = xsize
         self.width = ysize
         self.height = zsize
-        self.point = point
-        self.rotation = 0.0
+        self.frame = Frame(point, [1, 0, 0], [0, 1, 0])
 
     def __str__(self):
-        return f"Brick of {self.length}cm x {self.width}cm x {self.height}cm (rot: {self.rotation})"
+        return f"Brick of {self.length}cm x {self.width}cm x {self.height}cm (frame: {self.frame})"
 
     def draw(self):
         box = Box(self.length, self.width, self.height)
-        box.frame.point = self.point
-        rotation_in_radians = math.radians(self.rotation)
-        box.frame.rotate(rotation_in_radians, (0, 0, 1), self.point)
+        box.frame = self.frame.copy()
         return box
 
     def rotate(self, rotation_in_degrees):
-        self.rotation = rotation_in_degrees
+        self.frame.rotate(math.radians(rotation_in_degrees), (0, 0, 1), self.frame.point)
 
